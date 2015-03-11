ef0cd92ca30df0c77bb8143ea27aa55502a42f17
shsong97 <shsong97@gmail.com>
2015-03-11 22:29:41 +0900

    userlist, permisstion function added

--- apiserver/views.py
+++ apiserver/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 import json
 from django.core import serializers
+from django.contrib.auth.models import User
 # Create your views here.
 
 def login(request, userid, password):
@@ -11,7 +12,16 @@ def login(request, userid, password):
 	data['token']="token_value"
 	return HttpResponse(json.dumps(data), content_type="application/json")
 
-def tasks_json(request):
-    tasks = Task.objects.all()
-    data = serializers.serialize("json", tasks)
+def user_list(request):
+    userlist = User.objects.all()
+    data = serializers.serialize("json", userlist)
     return HttpResponse(data, content_type='application/json')	
+
+def perm(request, systemid, userid):
+	data = {}
+	data['userid']=userid
+	data['systemid']=systemid
+	data['token']="token_value"
+	data['programid']=["program1","program2","program3"]
+	
+	return HttpResponse(json.dumps(data), content_type="application/json")
